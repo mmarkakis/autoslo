85a8024da52e0682462883c27a56c129b474a616
Markos Markakis <mmarkakis98@gmail.com>
2026-01-13 09:59:04 -0500

    Update heavy hitter plots

--- src/autoslo/utils/colors.py
+++ src/autoslo/utils/colors.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 from cycler import cycler
 
 
-@dataclass(frozen=True)
 class Palette:
     white: str = "#FFFFFF"
     light_green: str = "#8FD694"
@@ -22,13 +21,47 @@ class Palette:
     gray: str = "#4A4A4A"
     black: str = "#000000"
 
-    def semantic_colors(self) -> Mapping[str, str]:
+    @staticmethod
+    def semantic_colors() -> Mapping[str, str]:
         return {
-            "success": self.dark_green,
-            "info": self.dark_blue,
-            "warning": self.dark_yellow,
-            "error": self.dark_red,
+            "success": Palette.dark_green,
+            "info": Palette.dark_blue,
+            "warning": Palette.dark_yellow,
+            "error": Palette.dark_red,
         }
+
+    # Expose as a colormap for seaborn/matplotlib
+    @staticmethod
+    def as_colormap() -> Mapping[str, str]:
+        return {
+            "light_green": Palette.light_green,
+            "dark_green": Palette.dark_green,
+            "light_blue": Palette.light_blue,
+            "dark_blue": Palette.dark_blue,
+            "light_yellow": Palette.light_yellow,
+            "dark_yellow": Palette.dark_yellow,
+            "light_orange": Palette.light_orange,
+            "dark_orange": Palette.dark_orange,
+            "light_red": Palette.light_red,
+            "dark_red": Palette.dark_red,
+            "light_gray": Palette.light_gray,
+            "gray": Palette.gray,
+            "black": Palette.black,
+        }
+    
+    @staticmethod
+    def as_list() -> list[str]:
+        return [
+            Palette.dark_blue,
+            Palette.dark_orange,
+            Palette.dark_green,
+            Palette.dark_red,
+            Palette.light_blue,
+            Palette.light_orange,
+            Palette.light_green,
+            Palette.light_red,
+            Palette.gray,
+        ]
 
 
 def set_plot_colors(dark: bool = False) -> None:
@@ -37,37 +70,35 @@ def set_plot_colors(dark: bool = False) -> None:
 
     Parameters:
         dark: If True, use darker colors; otherwise, use light colors.
-    
-    """
 
-    PALETTE = Palette()
+    """
 
     if dark:
         cycle = [
-            PALETTE.dark_blue,
-            PALETTE.dark_orange,
-            PALETTE.dark_green,
-            PALETTE.dark_red,
-            PALETTE.gray,
+            Palette.dark_blue,
+            Palette.dark_orange,
+            Palette.dark_green,
+            Palette.dark_red,
+            Palette.gray,
         ]
     else:
         cycle = [
-            PALETTE.light_blue,
-            PALETTE.light_orange,
-            PALETTE.light_green,
-            PALETTE.light_red,
-            PALETTE.gray,
+            Palette.light_blue,
+            Palette.light_orange,
+            Palette.light_green,
+            Palette.light_red,
+            Palette.gray,
         ]
 
     plt.rcParams.update(
         {
-            "figure.facecolor": PALETTE.white,
-            "axes.facecolor": PALETTE.light_green,
-            "text.color": PALETTE.black,
-            "axes.labelcolor": PALETTE.black,
-            "xtick.color": PALETTE.black,
-            "ytick.color": PALETTE.black,
-            "grid.color": PALETTE.gray,
+            "figure.facecolor": Palette.white,
+            "axes.facecolor": Palette.light_green,
+            "text.color": Palette.black,
+            "axes.labelcolor": Palette.black,
+            "xtick.color": Palette.black,
+            "ytick.color": Palette.black,
+            "grid.color": Palette.gray,
             "axes.prop_cycle": cycler("color", cycle),
         }
     )
