018a4f42107ad2b581741c89805d3c4926d9f7d1
Markos Markakis <mmarkakis98@gmail.com>
2026-02-01 17:45:09 -0500

    Som eupdated plotting notebooks

--- experiments/06_iconq_training/plot_train_val_test_accuracy.py
+++ experiments/06_iconq_training/plot_train_val_test_accuracy.py
@@ -173,6 +173,80 @@ def plot_qerror_single_barchart(
     ax.set_title(title)
     ax.legend(fontsize=fontsize)
 
+def plot_qerror_single_barchart_bounded(
+    ax: plt.Axes,
+    title: str,
+    split_true_y: dict[str, pd.Series],
+    split_predicted_y: dict[str, dict[str, ModelPrediction]],
+):
+
+    # Plot a grouped bar chart of p50, p90 and p95 Q-Errors for each split
+    metrics = {"p50": 0.5, "p90": 0.9, "p95": 0.95}
+    x = np.arange(len(metrics))  # the label locations
+    width = 0.25  # the width of the bars
+
+    fontsize = 20
+
+    colors = {
+        "train": Palette.light_green,
+        "val": Palette.light_blue,
+        "test": Palette.light_red,
+    }
+
+    for split_idx, split in enumerate(["train", "val", "test"]):
+        qerrors = []
+        true_y = split_true_y[split]
+        predicted_y = split_predicted_y[split]
+        for query_id, true_latency in true_y.items():
+            predicted_latency = predicted_y[query_id]  # type: ignore
+            pred_mean = predicted_latency.overall_mean_s()
+
+            is_censored = predicted_latency.metadata.get(
+                "target_is_lower_bound", False
+            )
+
+            if true_latency == 0 and pred_mean == 0:
+                qerror = 1.0
+            elif true_latency == 0 or pred_mean == 0:
+                qerror = float("inf")
+            elif is_censored and pred_mean > true_latency:
+                qerror = 1.0
+            else:
+                qerror = max(true_latency / pred_mean, pred_mean / true_latency)
+            qerrors.append(qerror)
+
+        p50 = pd.Series(qerrors).quantile(metrics["p50"])
+        p90 = pd.Series(qerrors).quantile(metrics["p90"])
+        p95 = pd.Series(qerrors).quantile(metrics["p95"])
+
+        ax.bar(
+            x + split_idx * width,
+            [p50, p90, p95],
+            width,
+            label=split.title(),
+            color=colors[split],
+        )
+
+        # Add a text label above each bar displaying its height
+        for metric_idx, metric in enumerate(metrics.keys()):
+            height = [p50, p90, p95][metric_idx]
+            ax.text(
+                x[metric_idx] + split_idx * width,
+                height + 0.05,
+                f"{height:.2f}",
+                ha="center",
+                va="bottom",
+                fontsize=fontsize,
+            )
+
+    ax.set_xticks(x + width)
+    ax.set_ylim(bottom=1, top=ax.get_ylim()[1] * 1.1)
+    ax.set_xticklabels(metrics.keys(), fontsize=fontsize)
+    ax.set_yticklabels(ax.get_yticks(), fontsize=fontsize)
+    ax.set_ylabel("Q-Error", fontsize=fontsize)
+    ax.set_title(title)
+    ax.legend(fontsize=fontsize)
+
 
 def plot_over_under_for_censored(
     ax: plt.Axes,
@@ -214,9 +288,6 @@ def plot_over_under_for_censored(
                 relative_error = float("inf")
             else:
                 relative_error = pred_mean / true_latency
-                print(
-                    f"Split: {split}, Query_id: {query_id}, Predicted: {pred_mean}, True: {true_latency}, Relative Error: {relative_error}"
-                )
 
             relative_errors.append(relative_error)
 
@@ -272,13 +343,12 @@ def main(iconq_model_id: str, hide_plot_title: bool):
         for i in range(len(split_dataset)):
             _, _, y, query_id, _, _, _ = split_dataset[i]
             true_y_d[query_id] = (
-                y.item() if not trained_on_log_runtime else np.expm1(y.item())
+                y.item() if not trained_on_log_runtime else np.exp(y.item())
             )
         split_true_y[split] = pd.Series(true_y_d)
 
         split_predicted_y[split] = model.predict_from_dataset(
             dataset=split_dataset,
-            use_stage_for_isolated_queries=use_stage_for_isolated_queries,
         )
 
         title = split.title() + " Set"
@@ -367,6 +437,29 @@ def main(iconq_model_id: str, hide_plot_title: bool):
         os.path.join(
             model_dir,
             f"iconq_model_{iconq_model_id}_qerror_barchart.svg",
+        )
+    )
+    plt.close(qerror_barchart_fig)
+
+    # Plot and post-process bounded Q-Error barchart figure
+    qerror_barchart_fig, qerror_barchart_ax = plt.subplots(
+        1, 1, figsize=(10, 6)
+    )
+    title = "Q-Error Summary Across Splits"
+    if use_stage_for_isolated_queries:
+        title += " (Stage Fallback)"
+    title += f" (Model {iconq_model_id})"
+    plot_qerror_single_barchart_bounded(
+        qerror_barchart_ax,
+        title if not hide_plot_title else "",
+        split_true_y,
+        split_predicted_y,
+    )
+    qerror_barchart_fig.tight_layout(rect=(0, 0.03, 1, 0.95))
+    qerror_barchart_fig.savefig(
+        os.path.join(
+            model_dir,
+            f"iconq_model_{iconq_model_id}_qerror_barchart_bounded.svg",
         )
     )
     plt.close(qerror_barchart_fig)
