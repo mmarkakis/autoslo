ba2c8bcb89f2595877fd31e2794a430b36d627de
Markos Markakis <mmarkakis98@gmail.com>
2026-04-13 18:09:16 -0400

    Move out `SimulatorEvent`

--- src/autoslo/workload_execution/workload_simulator.py
+++ src/autoslo/workload_execution/workload_simulator.py
@@ -44,29 +44,12 @@ from autoslo.utils.logging import (
 from autoslo.utils.yaml_helpers import dump
 from autoslo.workload_definition.query import Query
 from autoslo.workload_definition.workload import Workload
+from autoslo.workload_execution.simulator_event import (
+    SimulatorEvent,
+    SimulatorEventType,
+)
 
 _has_structured = lambda: bool(logging.getLogger(LOGGER_NAME).handlers)
-
-
-@dataclass
-class SimulatorEventType:
-    QUERY_ARRIVAL: str = "query_arrival"
-    QUERY_COMPLETION: str = "query_completion"
-    CAPACITY_CHECKPOINT: str = "capacity_checkpoint"
-    CLUSTER_READY: str = "cluster_ready"
-
-
-@dataclass
-class SimulatorEvent:
-    rel_time_s: float
-    event_type: str
-    details: dict[str, Any]
-
-    def __lt__(self, other: "SimulatorEvent") -> bool:
-        """Order by rel_time_s, then by event_type for tie-breaking."""
-        if self.rel_time_s == other.rel_time_s:
-            return self.event_type < other.event_type
-        return self.rel_time_s < other.rel_time_s
 
 
 class WorkloadSimulator:
